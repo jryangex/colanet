f80eda445ba6a9a7668dd7c035f8324d264bbde6
colaYang <M10815Q07@gapps.ntust.edu.tw>
2021-07-07 23:45:27 +0800

    fix mish source

--- package/model/module/activation.py
+++ package/model/module/activation.py
@@ -1,4 +1,14 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+class Mish(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #print("Mish activation loaded...")
+    def forward(self,x):
+        x = x * (torch.tanh(F.softplus(x)))
+        return x
 
 activations = {'ReLU': nn.ReLU,
                'LeakyReLU': nn.LeakyReLU,
@@ -7,6 +17,7 @@ activations = {'ReLU': nn.ReLU,
                'ELU': nn.ELU,
                'GELU': nn.GELU,
                'SiLU':nn.SiLU,
+               'Mish':Mish(),
                None: nn.Identity
                }
 
@@ -17,5 +28,10 @@ def act_layers(name):
         return nn.LeakyReLU(negative_slope=0.1, inplace=True)
     elif name == 'GELU':
         return nn.GELU()
+    elif name == 'SiLU':
+        return nn.SiLU()
+    elif name == 'Mish':
+        return Mish()
+
     else:
         return activations[name](inplace=True)
