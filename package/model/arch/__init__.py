d29312c81101b4846aa8510f1f6d56e67249c457
colaYang <m10815q07@gapps.ntust.edu.tw>
2021-08-12 17:56:28 +0800

    demov1

--- package/model/arch/__init__.py
+++ package/model/arch/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 from .gfl import GFL
-
+from .one_stage_detector import OneStageDetector
 
 def build_model(model_cfg):
     if model_cfg.arch.name == 'GFL':
@@ -9,7 +9,8 @@ def build_model(model_cfg):
                       "The name 'GFL' is deprecated, please change the model->arch->name "
                       "in your YAML config file to OneStageDetector. ")
         model = GFL(model_cfg.arch.backbone, model_cfg.arch.fpn, model_cfg.arch.head)
-
+    elif model_cfg.arch.name == 'OneStageDetector':
+        model = OneStageDetector(model_cfg.arch.backbone, model_cfg.arch.fpn, model_cfg.arch.head)
     else:
         raise NotImplementedError
     return model
